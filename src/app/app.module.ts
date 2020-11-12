176b58bec90030ea1f09a309684d6ad36a1f53e4
xXTime-OnXx <timon.schmid@bluewin.ch>
2020-11-12 15:05:38 +0100

    added firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {AngularFireModule} from '@angular/fire';
 import {AngularFirestoreModule} from '@angular/fire/firestore';
-import {AngularFireDatabaseModule} from '@angular/fire/database';
 import {AngularFireAuthModule} from '@angular/fire/auth';
 import {environment} from '../environments/environment';
 
