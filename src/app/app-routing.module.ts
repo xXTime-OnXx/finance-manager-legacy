64d216e430a2a58a33c7cede0bf8c06325568ab6
carolina <carolina.sanchez2001@gmail.com>
2020-11-25 21:10:24 +0100

    Added simple Login & Register Pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {RegisterComponent} from "./register/register.component";
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
   }
 ];
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,10 +14,12 @@ import {AngularFireAuthModule} from '@angular/fire/auth';
 import {environment} from '../environments/environment';
 import {AuthGuard} from "./core/auth.guard";
 import {AngularFireAuthGuardModule} from '@angular/fire/auth-guard';
+import {LoginComponent} from "./login/login.component";
+import {RegisterComponent} from "./register/register.component";
 
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, LoginComponent, RegisterComponent],
     entryComponents: [],
     imports: [
         BrowserModule,
