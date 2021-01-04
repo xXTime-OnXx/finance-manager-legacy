5c73334b4b88a768f1da2390fc5a12b5241a7a51
xXTime-OnXx <timon.schmid@bluewin.ch>
2021-01-05 00:33:08 +0100

    [FM-04] regex filtered ocr text to get bought products

--- src/app/page/ocr-scanner/ocr-scanner.page.ts
+++ src/app/page/ocr-scanner/ocr-scanner.page.ts
@@ -1,38 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {createWorker, Worker} from "tesseract.js";
 
 @Component({
-  selector: 'app-ocr-scanner',
-  templateUrl: './ocr-scanner.page.html',
-  styleUrls: ['./ocr-scanner.page.scss'],
+    selector: 'app-ocr-scanner',
+    templateUrl: './ocr-scanner.page.html',
+    styleUrls: ['./ocr-scanner.page.scss'],
 })
 export class OcrScannerPage implements OnInit {
 
-  image: '../../../assets/receipt.jpg';
-  ocrResult = '';
+    image: '../../../assets/receipt.jpg';
+    ocrResult = '';
 
-  private worker: Worker;
+    private worker: Worker;
 
-  constructor() {
-  }
+    constructor() {
+    }
 
-  async ngOnInit() {
-    await this.loadWorker();
-  }
+    async ngOnInit() {
+        await this.loadWorker();
+    }
 
-  async loadWorker() {
-    this.worker = createWorker({
-      logger: progress => {
-        console.log(progress);
-      }
-    });
-    await this.worker.load();
-    await this.worker.loadLanguage('eng')
-    await this.worker.initialize();
-  }
+    async loadWorker() {
+        this.worker = createWorker({
+            logger: progress => {
+                console.log(progress);
+            }
+        });
+        await this.worker.load();
+        await this.worker.loadLanguage('eng')
+        await this.worker.initialize();
+    }
 
-  async recognizeImage() {
-    const result = await this.worker.recognize('../../../assets/receipt.jpg');
-    console.log(result.data.text);
-  }
+    async recognizeImage() {
+        const result = await this.worker.recognize('/assets/receipt.jpeg');
+        const lines = result.data.text.split('\n');
+        const products = []
+        for (let line of lines) {
+            if (line.match(/^\d+.*\d+\.\d{0,2}$/g)) {
+                console.log('Matched: ' + line);
+            }
+        }
+        console.log(lines);
+    }
 }
