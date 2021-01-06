2e52074e6e564e3eacce390473f36ca5cf9caa51
xXTime-OnXx <timon.schmid@bluewin.ch>
2021-01-06 11:05:02 +0100

    [FM-02] added no_proxy env to karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,8 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 process.env.CHROME_BIN = require('puppeteer').executablePath();
+process.env.NO_PROXY = 'localhost, 0.0.0.0/4201, 0.0.0.0/9876';
+process.env.no_proxy = 'localhost, 0.0.0.0/4201, 0.0.0.0/9876';
 
 module.exports = function (config) {
     config.set({
