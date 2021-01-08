3fc05a354c94307389214bf0879906ff1ea3bd69
Carolina Sanchez <carolina_sanchez@sluz.ch>
2021-01-08 22:14:34 +0100

    FM-03: Added User Service Tests

--- src/app/service/user/user.service.spec.ts
+++ src/app/service/user/user.service.spec.ts
@@ -1,42 +1,99 @@
 import { TestBed } from '@angular/core/testing';
 
-import {AngularFirestore} from '@angular/fire/firestore';
+import {
+    AngularFirestore,
+    AngularFirestoreModule,
+    DocumentReference,
+    SETTINGS as FIRESTORE_SETTINGS
+} from '@angular/fire/firestore';
 import {UserService} from './user.service';
+import {AuthService} from '../auth/auth.service';
+import {AngularFireModule} from '@angular/fire';
+import {environment} from '../../../environments/environment';
+import firebase from 'firebase';
+import User = firebase.User;
 
 describe('UserService', () => {
-    //let service: UserService;
-    //let firestoreSpy: jasmine.SpyObj<AngularFirestore>;
+    let initialized = false;
+    let user: DocumentReference;
 
-    beforeEach(() => {
-        const spy = jasmine.createSpyObj('AngularFirestore', ['collection', 'doc']);
+    let service: UserService;
+    let firestore: AngularFirestore;
+    let authServiceSpy: jasmine.SpyObj<AuthService>;
+
+    beforeEach(async () => {
+        const authSpy = jasmine.createSpyObj('AuthService', ['getCurrentUser']);
 
         TestBed.configureTestingModule({
+            imports: [
+                AngularFireModule.initializeApp(environment.firebase),
+                AngularFirestoreModule,
+            ],
             providers: [
                 UserService,
-                { provide: AngularFirestore, useValue: spy }
+                {
+                    provide: FIRESTORE_SETTINGS,
+                    useValue: {host: 'localhost:8080', ssl: false}
+                },
+                {provide: AuthService, useValue: authSpy},
             ]
         });
 
-        //service = TestBed.inject(UserService);
-        //firestoreSpy = TestBed.inject(AngularFirestore) as jasmine.SpyObj<AngularFirestore>;
+        service = TestBed.inject(UserService);
+        firestore = TestBed.inject(AngularFirestore);
+        authServiceSpy = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
+
+        if (!initialized) {
+            user = await firestore.collection('user').add({
+                username: 'hans',
+                trips: []
+            });
+            initialized = true;
+        }
+    });
+
+    afterAll(async () => {
+        await user.delete();
     });
 
     it('should be created', () => {
-        expect(true).toBe(true);
+        expect(service).toBeTruthy();
     });
 
-    it('getUserOfTrip', () => {
-        // TODO: tbd. difficult cause of firebase chain-method-calls
-        expect(true).toBe(true);
-    });
+    it('createUser', async () => {
+        authServiceSpy.getCurrentUser.and.returnValue(Promise.resolve({uid: user.id} as User));
+        const createUserDto = {
+            userid: user.id,
+            username: 'hans',
+            trips: []
+        };
 
-    it('createUser', () => {
-        // TODO: tbd. difficult cause of firebase chain-method-calls
-        expect(true).toBe(true);
+        await service.createUser(createUserDto);
+
+        firestore.collection('user', ref => ref.where('userId', '==', createUserDto.userid))
+            .valueChanges()
+            .subscribe((userResult) => {
+                expect(userResult.length).toBe(1);
+            });
     });
 
     it('getUser', () => {
-        // TODO: tbd. difficult cause of firebase chain-method-calls
-        expect(true).toBe(true);
+        const result = service.getUser(user.id);
+
+        result.subscribe((userResult) => {
+            expect(userResult.id).toBe(user.id);
+        });
+    });
+
+    it('getUserByUsername', async () => {
+        await firestore.collection('user').add({
+            username: 'hans',
+            trips: []
+        });
+        const result = service.getUserByUsername('hans');
+
+        result.subscribe((userResult) => {
+            expect(userResult.length).toBe(1);
+        });
     });
 });
