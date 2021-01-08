03bbe7771bb5740540fbc74acde8caf5b9706dd1
xXTime-OnXx <timon.schmid@bluewin.ch>
2021-01-08 08:57:07 +0100

    [FM-05] updated dependencies in scanner service test file

--- src/app/service/scaner/scanner.service.spec.ts
+++ src/app/service/scaner/scanner.service.spec.ts
@@ -1,15 +1,37 @@
 import { TestBed } from '@angular/core/testing';
 
-import {AngularFirestore} from '@angular/fire/firestore';
+import {AngularFirestore, AngularFirestoreModule} from '@angular/fire/firestore';
 import { ScannerService } from './scanner.service';
+import {AuthService} from "../auth/auth.service";
+import {AngularFireModule} from "@angular/fire";
+import {environment} from "../../../environments/environment";
+import {SETTINGS as FIRESTORE_SETTINGS} from '@angular/fire/firestore';
 
 describe('ScannerService', () => {
   let service: ScannerService;
-  let firestoreSpy: jasmine.SpyObj<AngularFirestore>;
+  let firestore: AngularFirestore;
+  let authMock: jasmine.SpyObj<AuthService>
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const authSpy = jasmine.createSpyObj('AuthService', ['getCurrentUser']);
+
+    TestBed.configureTestingModule({
+      imports: [
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFirestoreModule,
+      ],
+      providers: [
+        ScannerService,
+        {
+          provide: FIRESTORE_SETTINGS,
+          useValue: {host: 'localhost:8080', ssl: false}
+        },
+        {provide: AuthService, useValue: authSpy},
+      ]
+    });
     service = TestBed.inject(ScannerService);
+    firestore = TestBed.inject(AngularFirestore);
+    authMock = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>
   });
 
   it('should be created', () => {
